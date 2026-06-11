5baa1a3680e39ff96df6a065e8ce712197eb9aaf
forax <forax@univ-mlv.fr>
2026-06-11 18:09:15 +0200

    message should only contains "low surrogate"

--- src/test/java/ToyJSONParserTest.java
+++ src/test/java/ToyJSONParserTest.java
@@ -176,7 +176,7 @@ public final class ToyJSONParserTest {
             """)
     );
 
-    assertTrue(ex.getMessage().contains("invalid low surrogate"));
+    assertTrue(ex.getMessage().contains("low surrogate"));
   }
 
   @Test
